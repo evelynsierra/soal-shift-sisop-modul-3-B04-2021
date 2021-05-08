577b2edd89f1e1f8ae7a9b0ad6e14f0174f29551
DidoFayed <80528848+DidoFayed@users.noreply.github.com>
2021-05-08 18:45:09 +0700

    Add files via upload

---
+++ Soal1/Server/server.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h> 
+#include <pthread.h> 
+#include <errno.h> 
+#include <unistd.h>
+#include <netinet/in.h> 
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <sys/time.h>
+#define PORT 8080
+
+int main(){
+    
+}
