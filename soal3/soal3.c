bbc5166d00504f18bbd3a218ae865365fe3236c8
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-05-22 20:02:52 +0700

    Update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -1,17 +1,32 @@
-#include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<pthread.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/stat.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 #include<sys/types.h>
-#include<dirent.h>
+#include<sys/stat.h>
+#include <syslog.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <pthread.h>
+#include <limits.h>
 
-pthread_t tid[100];
-char tempwd[1024];
 
-//mengecek file atau folder
+pthread_t tid[150];
+
+typedef struct args_struct {
+    char asal[500];
+    char cwd[500];
+} args_struct;
+
+int flag = 1;
+
+//cek apakah itu file
 int is_file(const char *path)
 {
     struct stat path_stat;
@@ -19,111 +34,217 @@ int is_file(const char *path)
     return S_ISREG(path_stat.st_mode);
 }
 
-//get extention of the file, file name, and moving file
-//opsi -f
 
-void* move(void *arg){
-    char *path;
-    char ext[100], dirname[100], dom[100];
-    char cut[100], fn[100];
-    path = (char*) arg;
-    strcpy(dom, arg);
-    strcpy(cut, arg);
+void movefileF(char *argv, char *cwd)
+{
+    int hiddenfile = 0;
+    char string[500];
+    strcpy(string, argv);
 
-    char *split, *arr[5], *split2, *arrn[5];
-    int n = 0;
-    split = strtok(path,"."); //split path
+    //baca filenya
+    int isfile = is_file(string);
+    char* tipe = strrchr(string, '.');
+    char* nama = strrchr(string, '/');
 
-
-    while(split != NULL)
+    if(nama[1] == '.')
     {
-        arr[n] = split;
-        n++;
-        split = strtok(NULL,".");
+        hiddenfile = 1; //is hidden file
+    }
+    
+    //lowercharacter
+    char tipeLow[100];
+    if(tipe)
+    {
+            strcpy(tipeLow, tipe);
+            for(int i = 0; tipeLow[i]; i++){
+                tipeLow[i] = tolower(tipeLow[i]);
+            }
+    }
+    else{
+        if(!isfile)
+        {
+            printf("This is folder\n");
+        }
+        else
+        {
+            strcpy(tipeLow, " Hidden"); // no extention
+        }
     }
 
-    if(n == 1)
+        if(hiddenfile)
+        {
+            strcpy(tipeLow," Hidden");
+        }
+        mkdir((tipeLow + 1), 0777); //folder with extention
+
+        size_t len = 0 ;
+        char a[500] ;
+        strcpy(a, argv);
+        char b[500] ;
+        strcpy(b, cwd);
+        strcat(b, "/");
+        strcat(b, tipeLow+1);
+        strcat(b, nama);
+        if (!rename(a,b)) {
+            printf("Berhasil Dikategorikan\n");
+        }
+        else
+        {
+            printf("Sad, Gagal :(\n");
+        }
+        remove(a);
+    
+}
+void movefileD(char *argv, char *cwd)
+{
+    int hiddenfile = 0;
+    char string[500];
+    strcpy(string, argv);
+    int isFile = is_file(string);
+    char* tipe = strrchr(string,'.');
+    char* nama = strrchr(string, '/');
+
+    if(nama[1] == '.')
     {
-        strcpy(ext, "Unknown");
+        hiddenfile = 1;
     }
 
+    char tipeLow[100];
+    if(tipe)
+    {
+            strcpy(tipeLow, tipe);
+            for(int i = 0; tipeLow[i]; i++){
+                tipeLow[i] = tolower(tipeLow[i]);
+            }
+    }
     else
     {
-        for(int a = 0; a < strlen(arr[n-1]); a++)
+        if(!isFile)
         {
-            ext[a] = tolower(arr[n-1][a]); //menyimpan seluruh extention
+            printf("This is a folder\n");
+            return;
+        }
+        else
+        {
+            strcpy(tipeLow, " Unknown"); //no extention
         }
     }
 
-    n = 0; 
-
-    //split direktori
-    split2 = strtok(cut, "/");
-    while(split2 != NULL)
+    if(hiddenfile)
     {
-        arrn[n] = split2;
-        n++;
-        split2 = strtok(NULL,"/");
+        strcpy(tipeLow, " Hidden");
     }
+    mkdir((tipeLow + 1), 0777);
 
-    strcpy(fn, arrn[n-1]);
+     size_t len = 0 ;
+        char a[1000] ;
+        strcpy(a, argv);
+        char b[1000] ;
+        strcpy(b, cwd);
+        strcat(b, "/");
+        strcat(b, tipeLow+1);
+        strcat(b, nama);
+        if (rename(a,b)) 
+        {
+            flag=0;
+        }
 
-    char cwd[1024];
-    getcwd(cwd, sizeof(cwd)); //mendapatkan direktori yang sedang dibuka
-    strcpy(dirname, cwd);
-    strcat(dirname, "/");
-    strcat(dirname, ext);
-    memset(ext,0,100);
-    mkdir(dirname,0777);
-
-//read mode on old file and write mode on new file
-
-    FILE *pf1, *pf2;
-    pf1 = fopen(dom,"r");
-    strcat(dirname, "/");
-    strcat(dirname, fn);
-    pf2 = fopen(dirname,"w");
-
-    if(!pf1)
-    {
-        printf("Unable to open source file to read\n");
-    }
-
-    if(!pf2)
-    {
-        printf("Unable to open target file to write\n");
-    }
-
-    int ch;
-    while((ch = fgetc(pf1)) != EOF)
-    {
-        fputc(ch, pf2); //memindahkan konten pada file lama
-    }
-
-    fclose(pf1);
-    fclose(pf2);
-    remove(dom); //menghapus file lama
-
-    return NULL;
+        remove(a);
 }
 
-int main(int argc, char *argv[])
+void *fcommand(void* arg)
 {
-    getcwd(tempwd, sizeof(tempwd));
+    args_struct args = *(args_struct*) arg;
+    movefileF(args.asal, args.cwd);
+    pthread_exit(0);
 
-    int i;
-    if(strcmp(argv[1], "-f") == 0)
+}
+
+void *dcommand(void* arg)
+{
+    args_struct args = *(args_struct*) arg;
+    movefileD(args.asal, args.cwd);
+    pthread_exit(0);
+
+}
+
+void sortfile(char *asal)
+{
+    args_struct args;
+    flag = 1;
+    strcpy(args.cwd, "/Users/evelynsierra/ITS/Semester_4/Sisop/soal-shift-sisop-modul-3-B04-2021-main/soal3/soal3");
+    DIR *dirp;
+    struct dirent *entry;
+    dirp = opendir(asal);
+    int index = 0;
+    while((entry = readdir(dirp)) != NULL)
     {
-        for(int i = 2; i < argc; i++)
+        if(entry->d_type == DT_REG)
         {
-            if(is_file(argv[i]))
+            char filename[305];
+            sprintf(filename,"%s/%s",asal, entry->d_name);
+            strcpy(args.asal,filename);
+
+            if(strcmp(filename,"/Users/evelynsierra/ITS/Semester_4/Sisop/soal-shift-sisop-modul-3-B04-2021-main/soal3/soal3/soal3.c") != 0)
             {
-                pthread_create(&(tid[i-2]), NULL, move, (void *)argv[i]); //create thread
+                pthread_create(&tid[index], NULL, dcommand, (void *)&args);
+                printf("%s\n", filename);
+                sleep(1);
+                index++;
             }
         }
-        for(int i = 0; i < argc - 2; i++)
+    }
+
+    if(!flag)
+    {
+        printf("Yah gagal disimpan\n");
+    }
+    else
+    {
+        printf("Direktori sudah disimpan\n");
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    args_struct args; //make struct
+    getcwd(args.cwd, sizeof(args.cwd));
+
+    if(strcmp(argv[1], "-f") == 0)
+    {
+        int count = 0;
+        for (int i = 2; i < argc; i++)
+        {
+            strcpy(args.asal, argv[i]);
+            //create thread
+            pthread_create(&tid[count], NULL, fcommand, (void *)&args);
+            sleep(1);
+            count++;
+        }
+        for(int i = 0; i < count; i++)
         {
             pthread_join(tid[i], NULL);
         }
     }
+
+    else if(strcmp(argv[1], "*") == 0)
+    {
+        char asal[] = "/Users/evelynsierra/ITS/Semester_4/Sisop/soal-shift-sisop-modul-3-B04-2021-main/soal3/soal3";
+
+        sortfile(asal);
+    }
+
+    else if(strcmp(argv[1], "-d") == 0)
+    {
+        char asal[1000];
+        strcpy(asal,argv[2]);
+        sortfile(asal);
+    }
+    else
+    {
+        printf("Perbaiki Argumen\n");
+        return 0;
+    }
+
+    return 0;
 }
